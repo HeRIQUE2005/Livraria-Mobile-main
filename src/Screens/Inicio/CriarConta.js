57a1f52b9294bf6f78506fb122385e53766dca1a
HENRIQUE DE CAMPOS FERREIRA <henrique.cferreira9@senacsp.edu.br>
2025-07-08 17:06:20 -0300

    Livraria-Mobile

--- src/Screens/Inicio/CriarConta.js
+++ src/Screens/Inicio/CriarConta.js
@@ -1,14 +1,100 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
 
-const CriarConta = () => {
+const CriarConta = ({navigation}) => {
+  const navegacao =()=>{
+    navigation.navigate("Login")
+  }
   return (
-    <View>
-      <Text>CriarConta</Text>
+    <View style={styles.container}>
+      <View style={styles.imagem}>
+        <Image
+          style={styles.posicao}
+          source={require("../../../assets/Logo.png")}
+        />
+      </View>
+      <View style={styles.criarContaContainer}>
+        <Text style={styles.criarConta}>crie sua conta</Text>
+      </View>
+      <View style={styles.LoginUsuarioContainer}>
+        <TextInput style={styles.TextInput} placeholder="email" />
+        <TextInput style={styles.TextInput} placeholder="Usuario" />
+        <TextInput style={styles.TextInput} placeholder="senha" />
+      </View>
+      <TouchableOpacity style={styles.CriarContainer}>
+        <Text style={styles.Criar}>Entrar</Text>
+      </TouchableOpacity>
+      <View style={styles.PageContaContainer}>
+        <Text>Não possui uma conta?</Text>
+        <TouchableOpacity onPress={navegacao}>
+          <Text>Entrar</Text>
+        </TouchableOpacity>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default CriarConta
+export default CriarConta;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+
+  imagem: {
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+  },
+
+  criarContaContainer: {
+    alignItems: "center",
+    marginTop: 20,
+  },
+
+  posicao: {
+    width: 150,
+  },
+
+  criarConta: {
+    fontSize: 20,
+  },
+
+  LoginUsuarioContainer: {
+    alignItems: "center",
+    marginTop: 20,
+    gap: 15,
+  },
+
+  TextInput: {
+    borderWidth: 1,
+    borderRadius: 8,
+    width: 200,
+    textAlign: "center",
+  },
+
+  CriarContainer: {
+    alignItems: "center",
+    marginTop: 66,
+    borderRadius: 8,
+    marginHorizontal: 130,
+    paddingVertical: 5,
+    backgroundColor: "#C4BBBB",
+  },
+  Criar: {
+    fontSize: 20,
+  },
+
+  PageContaContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 15,
+    flexDirection: "row",
+  },
+});
--- src/Screens/Inicio/Login.js
+++ src/Screens/Inicio/Login.js
@@ -1,11 +1,43 @@
-import { StyleSheet, Text, View, Image } from "react-native";
-import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
 
-const Login = () => {
+
+const Login = ({navigation}) => {
+  const navegacao =()=>{
+    navigation.navigate("CriarConta")
+  }
   return (
-    <View>
-      <View>
-        <Image source={require("../../../assets/Logo.png")} />
+    <View style={styles.container}>
+      <View style={styles.imagem}>
+        <Image
+          style={styles.posicao}
+          source={require("../../../assets/Logo.png")}
+        />
+      </View>
+      <View style={styles.criarContaContainer}>
+        <Text style={styles.criarConta}>Faça o Login</Text>
+      </View>
+      <View style={styles.LoginUsuarioContainer}>
+        <TextInput style={styles.TextInput} placeholder="email" />
+        <TextInput style={styles.TextInput} placeholder="senha" />
+      </View>
+      <TouchableOpacity style={styles.SenhaContainer}>
+        <Text>Esqueceu a Senha?</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.CriarContainer}>
+        <Text style={styles.Criar}>Entrar</Text>
+      </TouchableOpacity>
+      <View style={styles.PageContaContainer}>
+        <Text>Não possui uma conta?</Text>
+        <TouchableOpacity onPress={navegacao}>
+          <Text>Clique Aqui</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -13,4 +45,65 @@ const Login = () => {
 
 export default Login;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+
+  imagem: {
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+  },
+
+  criarContaContainer: {
+    alignItems: "center",
+    marginTop: 20,
+  },
+
+  posicao: {
+    width: 150,
+  },
+
+  criarConta: {
+    fontSize: 20,
+  },
+
+  LoginUsuarioContainer: {
+    alignItems: "center",
+    marginTop: 20,
+    gap: 15,
+  },
+
+  TextInput: {
+    borderWidth: 1,
+    borderRadius: 8,
+    width: 200,
+    textAlign: "center",
+  },
+
+  CriarContainer: {
+    alignItems: "center",
+    marginTop: 66,
+    borderRadius: 8,
+    marginHorizontal: 130,
+    paddingVertical: 5,
+    backgroundColor: "#C4BBBB",
+  },
+  Criar: {
+    fontSize: 20,
+  },
+
+  SenhaContainer: {
+    alignItems: "flex-end",
+    marginRight: 57,
+    marginTop: 14,
+  },
+
+  PageContaContainer: {
+    alignItems: "center",
+    justifyContent: 'center',
+    marginTop: 15,
+    flexDirection: "row",
+  },
+});
