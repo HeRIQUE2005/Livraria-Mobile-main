2e9c436cea902611d99ad45eeb151e64fe90dab8
HENRIQUE DE CAMPOS FERREIRA <henrique.cferreira9@senacsp.edu.br>
2025-07-14 17:09:59 -0300

    livraria-mobile-main

--- src/Components/Categoria.js
+++ src/Components/Categoria.js
@@ -1,9 +1,55 @@
-import { StyleSheet, View, Image, TextInput } from "react-native";
+import { StyleSheet, View, Image, TouchableOpacity } from "react-native";
 
-const Header = () => {
+const Categoria = () => {
     return (
-        <View>
-            
+        <View style={styles.Container}>
+            <View>
+
+                <TouchableOpacity onPress={Categoria}>
+                    <Image
+                        style={styles.LogoManga}
+                        source={require("../../assets/LogoManga.png")}
+                    />
+                </TouchableOpacity>
+
+
+            </View>
+
+            <View>
+            <TouchableOpacity>
+                <Image
+                    style={styles.LogoHq}
+                    source={require("../../assets/HqImagens.png")}
+                />
+            </TouchableOpacity>
+
+
+            </View>
         </View>
     )
-}
+};
+
+export default Categoria;
+
+const styles = StyleSheet.create({
+    Container: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+    },
+
+    LogoManga: {
+        alignItems: "center",
+        marginTop: 41,
+        marginHorizontal: 31
+
+    },
+    LogoHq: {
+        lignItems: "center",
+        marginTop: 36,
+        marginRight: 159
+    }
+
+
+
+})
--- src/Screens/Dashboard/Dashboard.js
+++ src/Screens/Dashboard/Dashboard.js
@@ -1,11 +1,13 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import Header from '../../Components/Header'
+import Categoria from '../../Components/Categoria'
 
 const Dashboard = () => {
   return (
     <View>
 <Header/>
+<Categoria/>
     </View>
   )
 }
